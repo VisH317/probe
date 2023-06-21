aec5e4dd5a2b602b7839bbf1ac15d88cc706d4a2
Vis <vt201916384@gmail.com>
2023-06-21 11:32:27 -0400

    setup basic test suite

---
+++ test/test.hpp
@@ -0,0 +1,22 @@
+#ifndef __TEST__
+#define __TEST__
+
+#include "layer.hpp"
+#include "network.hpp"
+
+// im gonna organize the test suite better later but for now its just a namespace
+
+namespace Test {
+    // layer tests
+    int layerConstructor();
+    int layerUUID();
+    int layerDiffUUID();
+
+    // network
+    int networkConstructor();
+    int networkForward();
+    int networkValid();
+}
+
+
+#endif
