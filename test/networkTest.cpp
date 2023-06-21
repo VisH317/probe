f83ba02472ef1bc5f3a0b0224b4bc8aa9b1f977c
Vis <vt201916384@gmail.com>
2023-06-21 11:50:46 -0400

    setup network forward tests

---
+++ test/networkTest.cpp
@@ -0,0 +1,31 @@
+#include "test.hpp"
+
+int Test::networkConstructor() {
+    std::vector<Layer> v{Layer(100, 50), Layer(50, 1)};
+    Network n(v);
+    return 0;
+}
+
+int Test::networkForward() {
+    std::vector<Layer> v{Layer(100, 50), Layer(50, 1)};
+    Network n(v);
+    torch::Tensor input = torch::ones(100);
+    torch::Tensor out;
+    out = n.forward(input);
+    return 0;
+}
+
+int Test::networkValid() {
+    std::vector<Layer> v{Layer(100, 50), Layer(50, 1)};
+    Network n(v);
+    bool one = n.checkValid();
+
+    std::vector<Layer> v{Layer(100, 50), Layer(51, 1)};
+    Network n2(v);
+    
+    bool two = n2.checkValid();
+
+    if(one && !two) return 0;
+    return 1;
+}
+
