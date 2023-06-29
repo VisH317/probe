036c1b31d0e95cd07e65a8c9e1f48272889722bb
Vis <vt201916384@gmail.com>
2023-06-29 16:06:48 -0400

    setup objective header

--- include/jitter/objective.hpp
+++ include/jitter/objective.hpp
@@ -2,16 +2,29 @@
 #define __OBJECTIVE_H__
 
 #include <torch/torch.h>
-#include "layer.hpp"
+#include "network.hpp"
 #include <iostream>
+#include <vector>
 
+// functor object that takes in a network pointer as a parameter, computes new objective function (distance from original), keeps track of all objective values
 
 class Objective {
     private:
-        Layer test;
+        Network* net;
+
+        /** List of output neurons being observed */
+        std::vector<int> idxs;
+
+        /** Input set to optimize on */
+        torch::Tensor input;
+
+        /** Concatenated list of all outputs based on the required output neurons being observed */
+        std::vector<torch::Tensor> outputs;  
 
     public:
-        Objective();
+        Objective(Network* net, std::vector<int>& idxs, torch::Tensor input) : net(net), idxs(idxs), input(input) {};
+
+        double loss();
         // void test();
 };
 
