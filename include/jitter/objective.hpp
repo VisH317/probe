2e86f5930a4af9f00bc8748e193d42f7ab4d8480
Vis <vt201916384@gmail.com>
2023-06-29 15:37:37 -0400

    refactoring AGAIN (hopefully for the last time)

---
+++ include/jitter/objective.hpp
@@ -0,0 +1,20 @@
+#ifndef __OBJECTIVE_H__
+#define __OBJECTIVE_H__
+
+#include <torch/torch.h>
+#include "layer.hpp"
+#include <iostream>
+
+
+class Objective {
+    private:
+        Layer test;
+
+    public:
+        Objective();
+        // void test();
+}
+
+
+
+#endif
---
+++ src/jitter/objective.cpp
@@ -0,0 +1,5 @@
+#include "objective.hpp"
+
+Objective::Objective() {
+    test = Layer(100, 50);
+}
