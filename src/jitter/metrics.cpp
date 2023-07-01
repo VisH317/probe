2ad9f8cb45ed4d84521ff7f2d193c802966fb779
Vis <vt201916384@gmail.com>
2023-07-01 18:46:31 -0400

    updating updatedist function

--- src/jitter/metrics.cpp
+++ src/jitter/metrics.cpp
@@ -4,11 +4,19 @@ void Metrics::addDist(std::string uuid) {
     dist[uuid] = { 50.0, 50.0 };
 }
 
+
 void Metrics::updateDist(std::string uuid, float lossUpdate, float randomChange) {
     std::pair<float, float>& orig = dist[uuid];
-    const float finalLoss = lossUpdate * randomChange * lambda1;
-    dist[uuid] = { orig.first+finalLoss, orig.second-finalLoss };
+    const float update = lossUpdate * randomChange * lambda1;
+
+    float one = orig.first+update;
+    float two = orig.second-update;
+    one += one<=1 ? 1 : one>=99 ? -1 : 0;
+    two += two<=1 ? 1 : two>=99 ? -1 : 0;
+
+    dist[uuid] = { one, two };
 }
+
 
 void Metrics::updateDist(std::string uuid, float lossUpdate, float prevAlpha, torch::Tensor weight, float randomChange) {
     torch::Tensor avgTen = torch::mean(weight, 0);
@@ -22,8 +30,9 @@ void Metrics::updateDist(std::string uuid, float lossUpdate, float prevAlpha, to
     one += one<=1 ? 1 : one>=99 ? -1 : 0;
     two += two<=1 ? 1 : two>=99 ? -1 : 0;
 
-    dist[uuid] = { one, orig.second-update };
+    dist[uuid] = { one, two };
 }
+
 
 void Metrics::sample(std::string uuid) {
     const std::pair<float, float>& params = dist[uuid];
