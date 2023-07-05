743c4b74dd519f40fb3b7484454f65e8c04a74ac
Vis <vt201916384@gmail.com>
2023-07-05 11:50:14 -0400

    setup response messages

---
+++ include/jitter/main/responseQueue.hpp
@@ -0,0 +1,10 @@
+#ifndef __RESPONSE_QUEUE_H__
+#define __RESPONSE_QUEUE_H__
+
+#include <queue>
+#include <mutex>
+#include "responseMessages.hpp"
+
+
+
+#endif
--- include/jitter/messages/responseMessages.hpp
+++ include/jitter/messages/responseMessages.hpp
@@ -1,8 +1,30 @@
 #ifndef __RESPONSE_MESSAGES_H__
 #define __RESPONSE_MESSAGES_H__
 
+#include <string>
 
+enum ResponseType {
+    UPDATE,
+    DONE
+};
 
+struct ResponseMessage {
+    virtual ResponseType getType();
+};
+
+struct ResponseUpdateMessage : ResponseMessage {
+    int workerId;
+    std::string uuid;
+    int layerNum;
+    double loss;
+    double randomChange;
+    ResponseType getType() override { return UPDATE; };
+};
+
+struct ResponseDoneMessage : ResponseMessage {
+    int workerId;
+    ResponseType getType() override { return DONE; };
+};
 
 
 #endif
