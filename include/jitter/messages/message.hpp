26e2507c6889e76768bd3686aaedba6d0024efe9
VisH317 <vt201916384@gmail.com>
2023-07-25 09:50:56 -0400

    setup message docs

--- include/jitter/messages/message.hpp
+++ include/jitter/messages/message.hpp
@@ -6,6 +6,10 @@
 #include <tuple>
 #include "network.hpp"
 
+/**
+ * @brief enum of message types for messages sent to worker queues
+ * 
+ */
 enum MessageType
 {
     START,
@@ -16,54 +20,119 @@ enum MessageType
     PARENT
 };
 
-// design question: should new networks be passed through messages each time or can the message calling be centralized to one manager
-// message passing: need to control move semantics and creation/deletion of other parameters
-// // important to have separate copies to prevent updates being run while a worker is running a forward on the thing
-// centralized manager has to manage both and both have similar memory requirements, since each worker still has to hold their own copy of the network
-
+/**
+ * @brief abstract class of message 
+ * 
+ */
 struct Message
 {
-    // int pid;
+    /**
+     * @brief get type of the message - used for finding which handler to call
+     * 
+     * @return MessageType
+     */
     virtual MessageType getType() = 0;
 };
 
 struct StartSearchMessage : public virtual Message
 {
+    /** id of neuron to start search on */
     std::string neuronID;
-    // std::pair<float, float> dist;
-    // Network net;
+
+    /**
+     * @brief Construct a new Start Search Message object
+     * 
+     * @param neuronID id of neuron
+     */
     StartSearchMessage(std::string neuronID) : neuronID(neuronID){};
+
+    /**
+     * @brief Get type of message
+     * 
+     * @return MessageType::START
+     */
     MessageType getType() override { return START; };
 };
 
 struct UpdateSearchMessage : Message
 {
+    /** List of neurons in the update message path */
     std::vector<std::string> neurons;
-    // std::pair<float, float> dist;
+
+    /** current layer num of the latest neuron being searched */
     int layerNum;
+
+    /**
+     * @brief Construct a new Update Search Message object
+     * 
+     * @param neurons - list of neurons in the update path
+     * @param layerNum - current layer of neuron
+     */
     UpdateSearchMessage(std::vector<std::string> neurons, int layerNum) : layerNum(layerNum), neurons(neurons){};
-    // Network net;
+    
+    /**
+     * @brief Get message type
+     * 
+     * @return MessageType::UPDATE
+     */
     MessageType getType() override { return UPDATE; };
 };
 
 struct ValidMessage : Message
 {
+    /** path of neurons to validate and continue */
     std::vector<std::string> neuronID;
+
+    /** Current layer num of latest neuron */
     int layerNum;
+
+    /**
+     * @brief Construct a new Valid Message object
+     * 
+     * @param neuronID - list of neuron IDs
+     * @param layerNum - layer number
+     */
     ValidMessage(std::vector<std::string> neuronID, int layerNum) : neuronID(neuronID), layerNum(layerNum){};
+
+    /**
+     * @brief Get message type
+     * 
+     * @return MessageType::VALID
+     */
     MessageType getType() override { return VALID; };
 };
 
 struct RejectedMessage : Message
 {
+    /** neuron path to reject */
     std::vector<std::string> neuronID;
+
+    /** Current layer number */
     int layerNum;
+
+    /**
+     * @brief Construct a new Rejected Message object
+     * 
+     * @param neuronID - neuron path to reject
+     * @param layerNum - current layer number
+     */
     RejectedMessage(std::vector<std::string> neuronID, int layerNum) : neuronID(neuronID), layerNum(layerNum){};
+
+    /**
+     * @brief Get message type
+     * 
+     * @return MessageType::REJECTED
+     */
     MessageType getType() override { return REJECTED; };
 };
 
 struct StopMessage : Message
 {
+    /**
+     * @brief Get message type
+     * 
+     * @return MessageType::STOP
+     */
     MessageType getType() override { return STOP; };
 };
 
--- include/jitter/messages/responseMessages.hpp
+++ include/jitter/messages/responseMessages.hpp
@@ -20,22 +20,69 @@ struct ResponseMessage
 
 struct ResponseUpdateMessage : ResponseMessage
 {
+    /** id of worker sending the message */
     int workerId;
+
+    /** list of neuron ids in path of update */
     std::vector<std::string, std::allocator<std::string>> uuid;
+
+    /** layer number of current updated neuron */
     int layerNum;
+
+    /** Loss at neuron */
     double loss;
+
+    /** Random induced shift in neuron that was measured */
     double randomChange;
+
+    /** Predicted update required to distribution based on weights change */
     double update;
+
+    /** Net iteration of jitter iteration */
     int netIteration;
+
+    /** Tensor of created updates */
     torch::Tensor updateTen;
+
+    /**
+     * @brief Construct a new Response Update Message object
+     * 
+     * @param workerId id of worker
+     * @param uuid neuron path
+     * @param layerNum layer number of updated neuron
+     * @param loss loss measured at updated neuron
+     * @param randomChange random change induced at the updated neuron
+     * @param update predicted distribution update
+     * @param netIteration iteration of network when update was induced
+     * @param ten update tensor used
+     */
     ResponseUpdateMessage(int workerId, std::vector<std::string> uuid, int layerNum, double loss, double randomChange, double update, int netIteration, torch::Tensor ten) : workerId(workerId), uuid(uuid), layerNum(layerNum), loss(loss), randomChange(randomChange), update(update), netIteration(netIteration), updateTen(ten) {};
+
+    /**
+     * @brief Get message type
+     * 
+     * @return ResponseType::RES_UPDATE 
+     */
     ResponseType getType() override { return ResponseType::RES_UPDATE; };
 };
 
 struct ResponseDoneMessage : ResponseMessage
 {
+    /** worker id */
     int workerId;
+
+    /**
+     * @brief Construct a new Response Done Message object
+     * 
+     * @param workerId worker id
+     */
     ResponseDoneMessage(int workerId) : workerId(workerId){};
+
+    /**
+     * @brief Get message type
+     * 
+     * @return ResponseType::RES_DONE
+     */
     ResponseType getType() override { return ResponseType::RES_DONE; };
 };
 
