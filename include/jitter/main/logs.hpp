c01cd9a1b5a2eede43b9d2a9e284619eaeb1cf9c
VisH317 <vt201916384@gmail.com>
2023-08-03 08:01:39 -0400

    docu

--- include/jitter/main/logs.hpp
+++ include/jitter/main/logs.hpp
@@ -7,13 +7,35 @@
 
 class Logger {
     private:
+
+        /** writing update logs file in csv format */
         std::ofstream file;
-        std::string fileName;
+
+        /** iteration log file (currently unused) */
         std::ofstream runtimeFile;
 
     public:
+
+        /**
+         * @brief Construct a new Logger object
+         * 
+         */
         Logger();
+
+        /**
+         * @brief Destroy the Logger object
+         * 
+         */
         ~Logger();
+
+        /**
+         * @brief Write an update to the updates.csv file
+         * 
+         * @param layer layer of neuron with update
+         * @param id id of neuron in layer that is being updated
+         * @param update update performed
+         * @param loss resulting loss from update
+         */
         void writeUpdate(int layer, std::string id, double update, double loss);
 };
 
